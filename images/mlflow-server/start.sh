276407adc057200de76ccae337fce863d345c3a5
Heet Sankesara <heetsankesara3@gmail.com>
2024-09-25 11:10:48 +0100

    minio changes

--- images/mlflow-server/start.sh
+++ images/mlflow-server/start.sh
@@ -4,7 +4,8 @@
 while ! nc -z ${S3_HOST} ${S3_PORT}; do echo 'Wait minio to startup...' && sleep 0.1; done; \
 sleep 5 && \
 mc config host add myminio ${MLFLOW_S3_ENDPOINT_URL} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} && \
-mc mb myminio/$AWS_BUCKET_NAME
+mc alias set myminio ${MLFLOW_S3_ENDPOINT_URL} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} && \
+mc mb myminio/$AWS_BUCKET_NAME --region $AWS_DEFAULT_REGION
 mc ls myminio
 echo "Created bucket ${AWS_BUCKET_NAME}"
 
@@ -14,5 +15,7 @@ mlflow db upgrade $DB_URI
 mlflow server \
     --backend-store-uri $DB_URI \
     --default-artifact-root $ARTIFACT_STORE \
+    --artifacts-destination $MLFLOW_S3_ENDPOINT_URL \
     --host $SERVER_HOST \
-    --port $SERVER_PORT
+    --port $SERVER_PORT \
+    --serve-artifacts
